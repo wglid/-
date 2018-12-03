3c321147183a904a51125a14d8becd74d01b4c9f
wglid <43250752+wglid@users.noreply.github.com>
2018-12-03 11:50:03 +0300

    Update laba9.c

--- laba9.c
+++ laba9.c
@@ -1,35 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
- 
+
 void main(void)
 {
-    char words[100][32], word[32];
-    FILE *finp;
-	FILE *fout; 
-    int count = 0, i, j;
-    if((finp = fopen ("input.txt", "r")) == NULL){
-        return;
-    }
-	fout = fopen("result.txt", "wt" );
-	if( fout == NULL )
+	char words[100][32], word[32];
+	FILE *finp;
+	FILE *fout;
+	int count = 0, i, j;
+	if ((finp = fopen("input.txt", "r")) == NULL) {
 		return;
-    /* Ñ÷èòûâàåì ñëîâà èç ôàéëà */
-    while (feof(finp) == 0 || count > 100)
-    {
-        fscanf (finp, "%s", word);
-        strcpy(words[count++], word);
-    }
-    fclose(finp);
-    /* Ñîðòèðîâêà ìåòîäîì ïóçûðüêà */
-    for(i = 1; i < count; i++)
-        for(j = 0; j < count - i; j++)
-            if(strcmp(words[j], words[j+1]) > 0){
-                strcpy(word, words[j]);
-                strcpy(words[j], words[j+1]);
-                strcpy(words[j+1], word);
-            }
-    for(i = 0; i < count; i++)
-        fprintf(fout,"%s\n", words[i]);
-    system("pause");
+	}
+	fout = fopen("result.txt", "wt");
+	if (fout == NULL)
+		return;
+	while (feof(finp) == 0 || count > 100)
+	{
+		fscanf(finp, "%s", word);
+		strcpy(words[count++], word);
+	}
+	fclose(finp);
+	for (i = 1; i < count; i++)
+		for (j = 0; j < count - i; j++)
+			if (strcmp(words[j], words[j + 1]) > 0) {
+				strcpy(word, words[j]);
+				strcpy(words[j], words[j + 1]);
+				strcpy(words[j + 1], word);
+			}
+	for (i = 0; i < count; i++)
+		fprintf(fout, "%s\n", words[i]);
+	system("pause");
 }
